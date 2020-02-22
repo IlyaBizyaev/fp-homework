7b8b4d504eb6b0a46d45f3a5204c30f5f1f02351
Ilya Bizyaev <bizyaev@zoho.com>
2020-02-22 21:37:03 +0300

    Add practice1 solution (not graded)

--- practice1/app/Main.hs
+++ practice1/app/Main.hs
@@ -27,7 +27,10 @@ module Main where
 -------------------------------
 
 
-main = interact solve
+main :: IO ()
+main = do
+  interact solve
+  putStrLn ""
 
 readInt :: String -> Int
 readInt = read
@@ -35,5 +38,50 @@ readInt = read
 showInt :: Int -> String
 showInt = show
 
+skipOneMatch :: (Eq a) => a -> [a] -> [a]
+skipOneMatch _ [] = []
+skipOneMatch y (x : xs) | x == y    = xs
+                        | otherwise = x : skipOneMatch y xs
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [] = []
+quickSort (x : xs) =
+  let smaller = quickSort [ a | a <- xs, a <= x ]
+      bigger  = quickSort [ a | a <- xs, a > x ]
+  in  smaller ++ [x] ++ bigger
+
+selectionSort :: (Ord a) => [a] -> [a]
+selectionSort [] = []
+selectionSort l =
+  let minimal = minimum l
+      rest    = skipOneMatch minimal l
+  in  minimal : selectionSort rest
+
+bubbleSort :: (Ord a) => [a] -> [a]
+bubbleSort l =
+  let
+    swapPass :: (Ord a) => [a] -> (Bool, [a])
+    swapPass []  = (False, [])
+    swapPass [x] = (False, [x])
+    swapPass (x0 : x1 : xs)
+      | x0 > x1
+      = (True, x1 : snd (swapPass (x0 : xs)))
+      | otherwise
+      = let passNext = swapPass (x1 : xs) in (fst passNext, x0 : snd passNext)
+    passResult = swapPass l
+  in
+    if fst passResult then bubbleSort (snd passResult) else snd passResult
+
+sort :: Int -> [Int] -> [Int]
+sort algorithm numbers = case algorithm of
+  0 -> quickSort numbers
+  1 -> selectionSort numbers
+  2 -> bubbleSort numbers
+  _ -> numbers
+
 solve :: String -> String
-solve s = "Hello " ++ s ++ "!"
+solve s =
+  let numbers = map readInt (words s)
+      sorted []       = []
+      sorted (x : xs) = sort x xs
+  in  unwords (map showInt (sorted numbers))
