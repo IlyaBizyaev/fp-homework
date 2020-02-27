e412b8ad293e7e4d892612b33e4147fee2ebd1bb
Ilya Bizyaev <bizyaev@zoho.com>
2020-02-27 23:36:00 +0300

    Fix factorial and Fibonacci for negative argument

--- hw0/src/Task4.hs
+++ hw0/src/Task4.hs
@@ -13,11 +13,19 @@ iterateElement x = fix (x :)
 
 -- | Calculate the nth Fibonacci number.
 fibonacci :: Integer -> Integer
-fibonacci = fix (\fib n -> if n > 1 then fib (n - 1) + fib (n - 2) else n)
+fibonacci =
+  let fib f n | n > 1     = f (n - 1) + f (n - 2)
+              | n < 0     = (-1) ^ (-n + 1) * f (-n)
+              | otherwise = n
+  in  fix fib
 
 -- | Calculate the factorial of n.
 factorial :: Integer -> Integer
-factorial = fix (\fact n -> if n > 1 then n * fact (n - 1) else 1)
+factorial =
+  let fact f n | n > 1     = n * f (n - 1)
+               | n < 0     = undefined
+               | otherwise = 1
+  in  fix fact
 
 -- | 'map' function implemented with the fixed point combinator.
 mapFix :: (a -> b) -> [a] -> [b]
